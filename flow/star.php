6e07138a87d0f1d6d3ae4bd66b6254e7e3411225
ChengFang Liu <juliet6124amy@gmail.com.tw>
2023-04-20 16:30:24 +0800

    stars

--- flow/star.php
+++ flow/star.php
@@ -1,5 +1,7 @@
 <style>
-
+*{
+    font-family: 'Courier New', Courier, monospace;
+}
 </style>
 <?php
 
@@ -17,6 +19,7 @@ for($i=1;$i<5;$i++){
     }
     echo "<br>";
 }
+echo "<hr>";
 for($i=0;$i<4;$i++){
     for($j=0;$j<$i;$j++){
         echo "&nbsp";
@@ -26,6 +29,50 @@ for($i=0;$i<4;$i++){
     for($j=0;$j<(3-$i);$j++){
         echo "*";
 
+    }
+    echo "<br>";
+}
+echo "<hr>";
+
+$n=7;
+
+for($i=0;$i<$n;$i++){
+    for($j=0;$j<$n;$j++){
+        if($i>0 && $i<($n-1) && $j>0 && $j<($n-1)){
+            echo "&nbsp";
+        }else{
+            echo "*";
+        }
+        
+
+    }
+    echo "<br>";
+}
+echo "<hr>";
+for($i=0;$i<$n;$i++){
+    for($j=0;$j<$n;$j++){
+        if($i==0 || $i==($n-1)){
+             echo "*";
+        }else if($j==0 || $j==$n-1){
+            echo "*";
+       }else{
+        echo "&nbsp";
+       }
+       
+    }
+    echo "<br>";
+}
+echo "<hr>";
+for($i=0;$i<$n;$i++){
+    for($j=0;$j<$n;$j++){
+        if($i==0 || $i==($n-1)){
+             echo "*";
+        }else if($j==0 || $j==$n-1 || $i==$j || $i==$n-$j-1){
+            echo "*";
+       }else{
+        echo "&nbsp";
+       }
+       
     }
     echo "<br>";
 }
