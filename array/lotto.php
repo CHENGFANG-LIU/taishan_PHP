37559a1a261826370e6f03efabce97ed63d0f861
劉承昉 <liuchengfang@liuchengfangdeMacBook-Air.local>
2023-04-22 22:45:09 +0800

    lotto

--- array/lotto.php
+++ array/lotto.php
@@ -1,4 +1,18 @@
 <?php
+
+// 大樂透有六個不重複號碼,從1號到38號
+
+
 $lotto=[];
-
-
+while(count($lotto)<6){
+    $ball=rand(1,38);
+    if(!in_array($ball,$lotto)){
+        $lotto[]=$ball;
+    }
+    echo "<pre>";
+    print_r($lotto);
+    echo "</pre>";
+}
+echo "<div>";
+    print_r($lotto);
+    echo "</div>";
